ef4f00643eb3d124937154b16f72f5b868a57ce2
wahaaandroid-droid <wahaaandroid@gmail.com>
2026-05-13 22:30:04 +0900

    Add more sample MIDI songs

--- src/data/sampleSongs.ts
+++ src/data/sampleSongs.ts
@@ -20,4 +20,20 @@ export const SAMPLE_SONGS: SampleSong[] = [
     title: "Only My Railgun Full",
     url: "/midi/only-my-railgun-full.mid",
   },
+  {
+    title: "U.N.オーエンは彼女なのか",
+    url: "/midi/un-owen-wa-kanojo-nanoka.mid",
+  },
+  {
+    title: "朝影",
+    url: "/midi/asakage.mid",
+  },
+  {
+    title: "夏影 -summer lights-",
+    url: "/midi/natsukage-summer-lights.mid",
+  },
+  {
+    title: "小さな手のひら",
+    url: "/midi/chiisana-te-no-hira.mid",
+  },
 ];
